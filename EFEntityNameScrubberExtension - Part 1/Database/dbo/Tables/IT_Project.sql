0a36dd184dd2d142da23f3acc87014ac0c66e23b
Michael Paterson <michael.l.paterson@gmail.com>
2012-06-25 21:08:54 -0400

    EFEntityNameScrubberExtension - Part 1

---
+++ EFEntityNameScrubberExtension - Part 1/Database/dbo/Tables/IT_Project.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[IT_Project] (
+    [Id]          INT           IDENTITY (1, 1) NOT NULL,
+    [ProjectName] VARCHAR (100) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ EFEntityNameScrubberExtension - Part 1/EFEntityNameScrubberExtension/EFEntityNameScrubber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using Microsoft.Data.Entity.Design.Extensibility;
+
+namespace EFEntityNameScrubberExtension
+{
+    [PartCreationPolicy(CreationPolicy.Shared)]
+    [Export(typeof(IModelGenerationExtension))]
+    public class EFEntityNameScrubber : IModelGenerationExtension
+    {
+        public void OnAfterModelGenerated(ModelGenerationExtensionContext context)
+        {
+            return;
+        }
+
+        public void OnAfterModelUpdated(UpdateModelExtensionContext context)
+        {
+            return;
+        }
+    }
+}
